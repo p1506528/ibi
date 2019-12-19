db9611400658468c5d7e88503fc1b42abd7314e3
Thomas Bourg <thomas.bourg@etu.univ-lyon1.fr>
2019-12-19 11:53:45 +0100

    Torch mlp
    Internet

--- torch mlp.py
+++ torch mlp.py
@@ -1,109 +1,103 @@
-#Travail de Thomas Bourg et Théo Buttez
-
-import gzip # pour décompresser les données
-import pickle # pour désérialiser les données
-import numpy as np# pour pouvoir utiliser des matrices
-import matplotlib.pyplot as plt # pour l'affichage
-import torch,torch.utils.data
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import gzip
+import pickle
 import random
-import math
-import statistics
-from sklearn.metrics import classification_report, confusion_matrix
+data = pickle.load(gzip.open('mnist_light.pkl.gz'),encoding='latin1')
+
+train_data = torch.Tensor(data[0][0])
+train_data_label = torch.Tensor(data[0][1])
+test_data = torch.Tensor(data[1][0])
+test_data_label = torch.Tensor(data[1][1])
+
+train_dataset = torch.utils.data.TensorDataset(train_data, train_data_label)
+test_dataset = torch.utils.data.TensorDataset(test_data, test_data_label)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+def test(net):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        # Enumerate the whole testing set
+        for d in test_loader:
+            image, label = d
+            # Get the net prediction
+            output = net(image)
+            total += 1
+            # Add 1 if correct, 0 otherwise
+            correct += int(np.argmax(output)) == (int(label[0]) if label[0].numpy().shape == () else int(np.argmax(label)))
+    # Return the accuracy of the net on the test data
+    return correct / total
+
+def fit(net, optimizer, criterion):
+    # For all epochs
+    for e in range(epochs):
+        print('epoch ' + str(e + 1) + ' starts')
+        # Enumerate the whole training set
+        for i, d in enumerate(train_loader, 0):
+            inputs, labels = d
+            # Reset the parameters gradients
+            optimizer.zero_grad()
+            # Get the net prediction
+            outputs = net(inputs)
+            # Compute the loss
+            loss = criterion(outputs, labels)
+            # Back propagate the gradient
+            loss.backward()
+            optimizer.step()
+            # Track the accuracy
+            total = labels.size(0)
+            _, predicted = torch.max(outputs.data, 1)
+            if not labels[0].numpy().shape == ():
+                _, labels = torch.max(labels, 1)
 
 
-def sigmoid(z):
-    s = 1 / (1 + np.exp(-z))
-    return s
+class LinearNet(nn.Module):
+    def __init__(self, f_s, a):
+        super(LinearNet, self).__init__()
+        # Define the activation function
+        self.act = a
+        # Define an array that will hold the layers's Torch variables
+        self.layers = []
+        # So that PyTorch can correctly detect all the variables, we first need 
+        # to initialize the variables and then add them to the array for later use
+        for i in range(len(f_s) - 1):
+            # Initialise the Torch variable of the current layer
+            exec('self.l_' + str(i) + ' = nn.Linear(f_s[' + str(i) + '], f_s[' + str(i + 1) + '])')
+            # Add the new variable to the array
+            exec('self.layers.append(self.l_' + str(i) + ')')
+        
+    def forward(self, x):
+        # Goes through each layer in a feed forward manner
+        # Gives the output through the activation function for 
+        # every layer except for the last one
+        for l in self.layers[:-1]:
+            x = self.act(l(x))
+        x = self.layers[-1](x)
+        return x
 
+# Parameters
+learning_rate = .001
+epochs = 1
+# The array where we define the structure of the net
+# We can add as many layers of any size as we want
+features_sizes = [784, 128, 64, 10]
+# Define the chosen activation function
+activation_fn = torch.nn.ReLU()
+# Define the chosen loss function
+criterion = nn.MSELoss()
 
-def compute_loss(Y, Y_hat):
+# Define the net and optimizer
+linear_net = LinearNet(features_sizes, activation_fn)
+optimizer = torch.optim.Adam(linear_net.parameters(), lr=learning_rate)
 
-    m = Y.shape[1]
-    L = -(1./m) * ( np.sum( np.multiply(np.log(Y_hat),Y) ) + np.sum( np.multiply(np.log(1-Y_hat),(1-Y)) ) )
+# Fit the net
+fit(linear_net, optimizer, criterion)
 
-    return L
-
-  
-if __name__ == '__main__':
-    # nombre d'image lues à chaque fois dans la base d'apprentissage (laisser à 1 sauf pour la question optionnelle sur les minibatchs)
-    TRAIN_BATCH_SIZE = 1
-    # on charge les données de la base MNIST
-    data = pickle.load(gzip.open('mnist_light.pkl.gz'),encoding='latin1')
-    # images de la base d'apprentissage
-    train_data = torch.Tensor(data[0][0])
-    # labels de la base d'apprentissage
-    train_data_label = torch.Tensor(data[0][1])
-    # images de la base de test
-    test_data = torch.Tensor(data[1][0])
-    # labels de la base de test
-    test_data_label = torch.Tensor(data[1][1])
-    train_dataset = torch.utils.data.TensorDataset(train_data,train_data_label)
-    test_dataset = torch.utils.data.TensorDataset(test_data,test_data_label)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=TRAIN_BATCH_SIZE, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
-    W1 = np.random.randn(100,28*28)
-    b1 = np.zeros((100, 1))
-    W2 = np.random.randn(1, 100)
-    b2 = np.zeros((1, 1))
-    learning_rate = 0.1
-    m = 28*28
-    pas = 100
-    
-    print('loaded')
-    
-    train = []
-    for image, label in train_loader:
-        image = image[0,:].numpy()
-        label = label[0,:].numpy()
-        train.append((image, label))
-
-    print('training')
-    
-    for i in range(pas):
-        print(i)
-        if i < len(train):
-            record = train[i]
-        else:
-            record = random.choice(train)
-        img = record[0]
-        label = record[1].reshape((1,10))
-        Z1 = np.matmul(W1, img) + b1
-        A1 = sigmoid(Z1)
-        Z2 = np.matmul(W2, A1) + b2
-        A2 = sigmoid(Z2)
-
-#        cost = compute_loss(label, A2)
-
-        dZ2 = A2-label
-        dW2 = (1./m) * np.matmul(dZ2, A1.T)
-        db2 = (1./m) * np.sum(dZ2, axis=1, keepdims=True)
-
-        dA1 = np.matmul(W2.T, dZ2)
-        dZ1 = dA1 * sigmoid(Z1) * (1 - sigmoid(Z1))
-        dW1 = (1./m) * np.matmul(dZ1, img.T)
-        db1 = (1./m) * np.sum(dZ1, axis=1, keepdims=True)
-
-        W2 = W2 - learning_rate * dW2
-        b2 = b2 - learning_rate * db2
-        W1 = W1 - learning_rate * dW1
-        b1 = b1 - learning_rate * db1
-
-        if i % 100 == 0:
-            print("Epoch", i, "cost: ", cost)
-
-    print("Final cost:", cost)
-    
-    print('test')
-    
-    Z1 = np.matmul(W1, X_test) + b1
-    A1 = sigmoid(Z1)
-    Z2 = np.matmul(W2, A1) + b2
-    A2 = sigmoid(Z2)
-
-    predictions = (A2>.5)[0,:]
-    labels = (y_test == 1)[0,:]
-
-    print(confusion_matrix(predictions, labels))
-    print(classification_report(predictions, labels))
-
-    
+# Print the accuracy on the test set
+linear_net.eval()
+acc = test(linear_net)
+print('Accuracy: ' + str(acc))
